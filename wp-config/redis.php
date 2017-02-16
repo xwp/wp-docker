9d09af0cfb4e3e7c49c0d9df398d96a48d1a047f
Derek Herman <derek@valendesigns.com>
2017-02-15 17:13:12 -0800

    Add Redis Object & Page caching

---
+++ wp-config/redis.php
@@ -0,0 +1,11 @@
+<?php
+
+define( 'WP_CACHE', true );
+
+global $redis_server;
+$redis_server = array(
+	'host'     => 'redis',
+	'port'     => 6379,
+	'auth'     => 'redis',
+	'database' => 0,
+);
---
+++ wp-content/object-cache.php
@@ -0,0 +1,3 @@
+<?php
+# This is a Windows-friendly symlink :-)
+require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
