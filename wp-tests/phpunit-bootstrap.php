d17fa0b8b2cef349d894296a9857598fb3d65756
Derek Herman <derek@valendesigns.com>
2017-03-20 20:22:53 -0700

    Tests path

--- wp-tests/phpunit-bootstrap.php
+++ wp-tests/phpunit-bootstrap.php
@@ -22,6 +22,10 @@ if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
 }
 require_once $_tests_dir . '/includes/functions.php';
 
+// @codingStandardsIgnoreStart
+echo $_tests_dir;
+// @codingStandardsIgnoreEnd
+
 // Setup the plugins.
 if ( '/tmp/wordpress-tests' === $_tests_dir ) {
 	$_plugins_array = glob( getcwd() . '/wp-content/plugins/*' );
